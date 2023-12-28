753137d1e75e87ebcb59634d32815699242d41ee
Shivam Shahi <shivam.shahi06@gmail.com>
2023-12-28 23:30:21 +0530

    started work on server

---
+++ server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    
+)
+
+func main() {
+    fmt.Println("Hello, world!")
+
+    // Example of using a function
+    myFunction()
+}
+
+// Define other functions or types as needed
+func myFunction() {
+    fmt.Println("This is an example function.")
+}
